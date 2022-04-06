82da6783f1281e7873302497f9bfcc1c87359e8d
Necolanch <NICruz@student.fullsail.edu>
2022-04-06 12:24:01 -0400

    Adjust class names in HTML to add scrollIntoView for other sections, add touchstart event to test scrollIntoView on mobile

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,14 @@ const aboutSection = document.querySelector(".aboutMe");
 aboutTab.addEventListener("click",()=>{
     aboutSection.scrollIntoView({behavior:"smooth"});
 })
+
+aboutTab.addEventListener("touchstart",()=>{
+    aboutSection.scrollIntoView({behavior:"smooth"});
+})
+
+const xpTab = document.querySelector(".xpTab");
+const xpSection = document.querySelector(".xp");
+
+xpTab.addEventListener("click",()=>{
+    xpSection.scrollIntoView({behavior:"smooth"});
+})
